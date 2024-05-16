153735bf6bee02a9575197ad2d5edb88d6760883
imanfsx <iman30039581@gmail.com>
2024-05-16 18:20:18 +1000

    trying to fix pak and sydney

--- src/index.js
+++ src/index.js
@@ -7,9 +7,9 @@ function updateTime() {
   );
   let currentLocation = moment.tz.guess();
 
-  let currentLocationTime = moment().tz(`${currentLocation}`).format("H:mm:ss");
+  let currentLocationTime = moment().tz(currentLocation).format("H:mm:ss");
   let currentLocationDate = moment()
-    .tz(`${currentLocation}`)
+    .tz(currentLocation)
     .format("dddd, Do YYYY");
 
   currentLocationTimeElement.innerHTML = currentLocationTime;
@@ -20,47 +20,22 @@ function updateTime() {
     let lahoreDateElement = lahoreElement.querySelector(".date");
     let lahoreTimeElement = lahoreElement.querySelector(".time");
 
-    let lahoreDate = moment().tz("Asia/Lahore").format("dddd, Do YYYY");
-    let lahoreTime = moment().tz("Asia/Lahore").format("H:mm:ss");
+    let lahoreTime = moment().tz("Asia/Karachi").format("H:mm:ss");
+    let lahoreDate = moment().tz("Asia/Karachi").format("dddd, Do YYYY");
 
     lahoreDateElement.innerHTML = lahoreDate;
     lahoreTimeElement.innerHTML = lahoreTime;
   }
+
   let sydneyElement = document.querySelector("#sydney");
   if (sydneyElement) {
     let sydneyDateElement = sydneyElement.querySelector(".date");
     let sydneyTimeElement = sydneyElement.querySelector(".time");
-    let sydneyDate = moment().tz("Australia/Sydney").format("dddd, Do YYYY");
+
     let sydneyTime = moment().tz("Australia/Sydney").format("H:mm:ss");
+    let sydneyDate = moment().tz("Australia/Sydney").format("dddd, Do YYYY");
 
     sydneyDateElement.innerHTML = sydneyDate;
     sydneyTimeElement.innerHTML = sydneyTime;
   }
 }
-
-function updateCity(event) {
-  let cityTimeZone = event.target.value;
-  if (cityTimeZone === "current") {
-    cityTimeZone = moment.tz.guess();
-  }
-  let citiesElement = document.querySelector("#cities");
-  let cityName = cityTimeZone.replace("_", " ").split("/")[1];
-  let cityTime = moment().tz(cityTimeZone);
-  if (cityTimeZone.length > 0) {
-    citiesElement.innerHTML = `<li class="city">
-          <div>
-            <h2> ${cityName}</h2>
-            <div class="date">${cityTime.format("dddd, Do YYYY")}</div>
-          </div>
-          <div class="time">${cityTime.format("H:mm")}</div>
-        </li>
-        <a  href = " / ">Home</a>`;
-  } else {
-    citiesElement.innerHTML = `<a href=" / ">Home</a>`;
-  }
-}
-let selectCityElement = document.querySelector("#select-city");
-selectCityElement.addEventListener("change", updateCity);
-
-updateTime();
-setInterval(updateTime, 1000);
